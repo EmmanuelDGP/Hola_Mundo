758271e87e0f7a0ac3e3f9a9e93e510b71d73bd0
Emmanuel <Emmanuel.Polanco27@gmail.com>
2021-10-14 01:06:09 -0400

    Se quito la linea Inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,8 +3,6 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
-        //emmanuel esto es un comentario solo uno recuerda comentar tu codigo
-        //System.out.println("Esta instruccion es una prueba");
         return "%s, %s".formatted(apellidos, nombre);
     }
 }
