41ac8af0aeef94df620ff18fb519c2c71c0b7943
EmmanuelDGP <92494153+EmmanuelDGP@users.noreply.github.com>
2021-10-14 01:00:54 -0400

    comentario

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,8 +3,8 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
-        //emmanuel esto es un comentari solo uno recuerda comentar tu codigo
-        System.out.println("Esta instruccion es una prueba");
+        //emmanuel esto es un comentario solo uno recuerda comentar tu codigo
+        //System.out.println("Esta instruccion es una prueba");
         return "%s, %s".formatted(apellidos, nombre);
     }
 }
