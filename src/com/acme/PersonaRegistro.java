db8b0ea7c4c5385d59e833fa50d84280d8db2b0c
EmmanuelDGP <92494153+EmmanuelDGP@users.noreply.github.com>
2021-10-14 00:59:55 -0400

    Prueba

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,6 +3,7 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
+        //emmanuel esto es un comentari solo uno recuerda comentar tu codigo
         System.out.println("Esta instruccion es una prueba");
         return "%s, %s".formatted(apellidos, nombre);
     }
